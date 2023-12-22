6cfda392a804fb7a43b2d1b88f8c24fcd93037df
doenst1matter <1doesnt.matter1@mail.ru>
2023-12-22 20:18:41 +0300

    BASIC. Modified LogService: Record function.

--- Services/LogService.js
+++ Services/LogService.js
@@ -7,10 +7,12 @@ class LogService {
         const dt = DateService.Now();
         const pid = IDService.GenerateNumericID();
         const session = req.session;
-        const path = `./logs/requests/${dt.fullDate}${req.originalUrl.replaceAll("/", "-")}.log`
+
+        const path = `./logs/requests${req.originalUrl}`;
+        const filename = `${dt.fullDate}${req.originalUrl.replaceAll("/", "-")}.log`;
         const log = `${dt.full},${pid},${session.os.name}:${session.device},${session.browser.name}:${session.browser.version},${req.method},${req.originalUrl}`
         
-        LogService.Record(path, log);
+        LogService.Record(path, filename, log);
 
         req.pid = pid;
         console.log(log);
@@ -18,30 +20,42 @@ class LogService {
 
     LogResponse(req, res) {
         const dt = DateService.Now();
-        const path = `./logs/responses/${dt.fullDate}${req.originalUrl.replaceAll("/", "-")}.log`
+        const path = `./logs/responses${req.originalUrl}`;
+        const filename = `${dt.fullDate}${req.originalUrl.replaceAll("/", "-")}.log`;
         const log = `${dt.full},${req.pid},${res.statusCode},${res.statusMessage}`;
 
-        LogService.Record(path, log);
+        LogService.Record(path, filename, log);
 
         console.log(log + "\n");
     }
 
     LogError(req, error) {
         const dt = DateService.Now();
-        const path = `./logs/errors/${dt.fullDate}${req.originalUrl.replaceAll("/", "-")}.log`
-        const log = `${dt.full},${req.pid},${error.statusCode},${error.type},${error.message}${error.statusCode >= 500 ? ","+error.stack : ""}`;
+        const path = `./logs/errors${req.originalUrl}`;
+        const filename = `${dt.fullDate}${req.originalUrl.replaceAll("/", "-")}.log`;
+        const log = `${dt.full},${req.pid},${error.statusCode},${error.type},${error.message}${error.statusCode >= 500 ? "," + error.stack : ""}`;
 
-        LogService.Record(path, log)
+        LogService.Record(path, filename, log);
 
         console.log(log);
     }
 
-    static Record(path, log) {
-        if(FS.existsSync(path)) {
-            FS.appendFileSync(path, log + "\n");
+    static Record(path, filename, log) {
+        const isFileExists = FS.existsSync(`${path}/${filename}`);
+        const isDirExists = FS.existsSync(`${path}`);
+
+        if(!isDirExists) {
+            path.split("/").reduce((accum, current) => {
+                if(!FS.existsSync(`${accum}/${current}`)) FS.mkdirSync(`${accum}/${current}`, (error) => console.log(error))
+                return `${accum}/${current}`;
+            })
+        }
+        
+        if(isFileExists) {
+            FS.appendFileSync(`${path}/${filename}`, log + "\n");
         }
         else {
-            FS.writeFileSync(path, log + "\n");
+            FS.writeFileSync(`${path}/${filename}`, log + "\n");
         }
     }
 }
